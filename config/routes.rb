c57c667f9e916e1ca62b4a598c5ea8bdc8748e22
jordi92500 <jordi92500@hotmail.fr>
2015-03-02 10:21:28 +0100

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+ 
+
   get 'static_pages' => 'static_pages#home'
   
   root :to => 'static_pages#home'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'login' => 'static_pages#login'
   get 'contact' => 'static_pages#contact'
-
+  get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
