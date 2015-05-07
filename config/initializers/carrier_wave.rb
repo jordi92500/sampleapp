d9d2677112515d307566a6e82a949fc1865fff16
jordi92500 <jordi92500@hotmail.fr>
2015-05-07 22:24:09 +0200

    production mode

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
 
 
-#if Rails.env.production?
-#require 'rubygems'
-#require 'fog'
+if Rails.env.production?
+require 'rubygems'
+require 'fog'
 
-#CarrierWave.configure do |config|
- # config.cache_dir = "#{Rails.root}/tmp/uploads"
-#end
+CarrierWave.configure do |config|
+ config.cache_dir = "#{Rails.root}/tmp/uploads"
+end
 
-#end
+end
