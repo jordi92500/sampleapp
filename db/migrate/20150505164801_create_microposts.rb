2bee7e6c103b4ece6ae761d052cf24a049bcd949
jordi92500 <jordi92500@hotmail.fr>
2015-05-08 16:03:48 +0200

    problem with migrating fixed

--- db/migrate/20150505164801_create_microposts.rb
+++ db/migrate/20150505164801_create_microposts.rb
@@ -1,13 +1,11 @@
-class CreateRelationships < ActiveRecord::Migration
+class CreateMicroposts < ActiveRecord::Migration
   def change
-    create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+    create_table :microposts do |t|
+      t.text :content
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
+    add_index :microposts, [:user_id, :created_at]
   end
 end
